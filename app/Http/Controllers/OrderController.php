df8036011ba33000e0ed41457b49ae1cef7c8fc7
Tom Penzer <tpenzer@mailcan.com>
2018-09-05 11:25:12 -0700

    Fix class name of order controller, so the stores load as expected

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,7 +8,7 @@ use Cache;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-class StoreController extends Controller
+class OrderController extends Controller
 {
     const CACHE_TAG = 'orders';
 

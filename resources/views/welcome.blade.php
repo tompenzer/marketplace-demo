e8929ef5f2deb592a304a285c05e1771e831194d
Tom Penzer <tpenzer@mailcan.com>
2018-10-08 14:11:05 -0700

    Switch to using bootstrap-sass to load Bootstrap styles, enabling customization.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,6 @@
     	<title>{{ config('app.name', 'Laravel') }}</title>
 
     	<!-- Styles -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
     	<!-- Scripts -->
     	<script>
     		window.Laravel = {!! json_encode([
